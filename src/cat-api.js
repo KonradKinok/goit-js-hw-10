a015dc528a453721d6d9e1932cb5c8271ea70dc7
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2024-04-18 23:09:48 +0200

    18.04.2024

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,6 +1,3 @@
 'use strict';
-import * as catsApi from './cat-api';
-
-const api = catsApi.apiKey;
-const apiKey =
+export const apiKey =
   'live_CxkNmiyyb6IlYY7a06bc6CPiONxdV78sZd9uarhcRkpu9pj6qYequ5IHvIxpkRWC';
